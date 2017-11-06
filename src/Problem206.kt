9fb10daffefa36481c71a8752f7eeebc8684b054
Danny Rademacher <danny.rademacher@gmail.com>
2017-11-06 01:22:45 +0100

    imported and formatted file for project 345

--- src/Problem206.kt
+++ src/Problem206.kt
@@ -1,21 +1,29 @@
 import java.math.BigInteger
 
 fun main(args: Array<String>) {
-    
-    for (i in 1010101010..1389026623) {
-        val k = i * i
+
+    for (i in 1010101010L..1389026623L) {
+        val k = BigInteger.valueOf(i) * BigInteger.valueOf(i)
         if (check(k)) {
-            println("Result: " + k)
+            println("Result: " + i)
             break
         }
     }
-
-    println("hi")
 }
 
-fun check(n : Int) : Boolean {
-    if (n % 10 != 0) {
+fun check(m : BigInteger) : Boolean {
+    var n = m
+
+    if (n.rem(BigInteger.valueOf(10)) != BigInteger.valueOf(0)) {
         return false
+    }
+
+    for (i in 9L downTo 1L)  {
+        n /= BigInteger.valueOf(100)
+
+        if (n.rem(BigInteger.valueOf(10)) != BigInteger.valueOf(i)) {
+            return false
+        }
     }
 
     return true
