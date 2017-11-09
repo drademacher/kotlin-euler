05573e5ee274c1bcccd6070fa569d1ccedce7769
Danny Rademacher <danny.rademacher@gmail.com>
2017-11-09 01:50:17 +0100

    prime sieve with (2,3) wheel factorization

--- src/util.kt
+++ src/util.kt
@@ -12,3 +12,24 @@ fun binomial(N: Int, K: Int): BigInteger {
 
     return ret
 }
+
+
+/**
+ * Retrieve a function which tells whether a given input number is prime
+ *
+ * how to increase wheel
+ * update: sieve array size, inWheel(), toWheel(), return function
+ */
+fun getPrimeTester(n : Int) : (Int) -> Boolean {
+    val sieve = BooleanArray(n/3 + 1, {it != 0})
+
+    fun inWheel(k : Int) = k % 6 == 1 || k % 6 == 5
+    fun toWheel(k : Int) = k / 3
+//    fun toReal(k : Int) = 6 * (k/2) + 1 + (k % 2) * 4
+
+    (1..Math.sqrt(n.toDouble()).toInt() step 2)
+            .filter { inWheel(it) && sieve[toWheel(it)] }
+            .forEach { i -> (i * i..n step i).filter { inWheel(it) }.forEach { j -> sieve[toWheel(j)] = false } }
+
+    return { it -> it == 2 || it == 3 || (it in 1..n && inWheel(it) && sieve[toWheel(it)])}
+}
